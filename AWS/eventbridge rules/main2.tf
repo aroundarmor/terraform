ac92b8e533e046ab16863d22325ba4120d3a9ecb
Sourav Patel <37928615+aroundarmor@users.noreply.github.com>
2024-01-31 12:23:42 +0530

    Update main2.tf

--- AWS/eventbridge rules/main2.tf
+++ AWS/eventbridge rules/main2.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region = "us-west-2"
   access_key = "access_key"
   secret_key = "secret_key"
+  default_tags {
+    tags = {
+      Service = "EventBridgeAlert"
+      Environment = "Dev"
+    }
+  }
 }
 
 #creating eventbridge event rule
