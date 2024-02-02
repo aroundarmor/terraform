3e986a2b8b6b1ba524e655255172a6268e32961e
aroundarmor <sourav53sd@gmail.com>
2024-02-02 12:53:51 +0530

    vm-create-code-updated-2

--- AWS/VM/vm.tf
+++ AWS/VM/vm.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "dummy1" {
   ami = "ami-0277155c3f0ab2930"
   instance_type = var.instance_type
-  vpc_security_group_ids = ["security-group"]
+  #vpc_security_group_ids = ["security-group"]
   
 }
